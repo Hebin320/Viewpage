a95eaa3744d5ef202200de91c16c38213a41d048
Hebin320 <1083019222@qq.com>
2017-02-20 17:14:21 +0800

    commit

--- app/src/main/java/com/hebin/viewpager/MainActivity.java
+++ app/src/main/java/com/hebin/viewpager/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends FragmentActivity {
 
     MyViewpager vpMain;
     LinearLayout llPoint;
+    ViewPager vpMain_1;
+    LinearLayout llPoint_1;
     private int[] img = {R.mipmap.ic_vp_01, R.mipmap.ic_vp_02, R.mipmap.ic_vp_03};
 
     @Override
@@ -20,6 +22,8 @@ public class MainActivity extends FragmentActivity {
         setContentView(R.layout.activity_main);
         vpMain = (MyViewpager) findViewById(R.id.vp_main);
         llPoint = (LinearLayout) findViewById(R.id.ll_point);
+        vpMain_1 = (ViewPager) findViewById(R.id.vp_main_1);
+        llPoint_1 = (LinearLayout) findViewById(R.id.ll_point_1);
         ArrayList<Fragment> list = new ArrayList<>();
         ImgFragment imgFragment;
         imgFragment = ImgFragment.newInstance(img[img.length - 1]);
@@ -35,6 +39,22 @@ public class MainActivity extends FragmentActivity {
         vpMain.setFm(getSupportFragmentManager());
         vpMain.setTime(800);
         vpMain.setFragment(list);
+        //第二个Viewpager
+        ArrayList<Fragment> list_1 = new ArrayList<>();
+        ImgFragment imgFragment_1;
+        imgFragment_1 = ImgFragment.newInstance(img[img.length - 1]);
+        list_1.add(imgFragment_1);
+        for (int i = 0; i < img.length; i++) {
+            imgFragment_1 = ImgFragment.newInstance(img[i]);
+            list_1.add(imgFragment_1);
+        }
+        imgFragment_1 = ImgFragment.newInstance(img[0]);
+        list_1.add(imgFragment_1);
+        VpAdapter vpAdapter = new VpAdapter(getSupportFragmentManager(),list_1);
+        vpMain_1.setAdapter(vpAdapter);
+        vpMain_1.setCurrentItem(1);
+        vpMain_1.setOffscreenPageLimit(img.length-1);
+        vpMain_1.addOnPageChangeListener(new VpListener(VpListener.getImageViews(this,llPoint_1,list_1),vpMain_1,1600));
 
     }
 
